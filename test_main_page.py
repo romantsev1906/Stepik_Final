d1636bae5de812aa3b83f013e4934e8db4589866
romantsev1906 <gulievroman82@gmail.com>
2026-04-08 18:51:50 +0300

    Final commit for review Final Project
    Final commit for review Final Project

---
+++ test_main_page.py
@@ -0,0 +1,29 @@
+import pytest
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+
+main_base_link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, main_base_link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, main_base_link)
+        page.open()
+        page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, main_base_link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_basket_description_message()
+    basket_page.should_be_basket_empty_message()
